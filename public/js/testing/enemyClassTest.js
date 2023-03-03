a1aa47e2b53ebac2f432b95ed2bac5d14641bbd9
sonadro <sondre.rooth@gmail.com>
2023-03-03 12:18:12 +0100

    Basic functional game loop

--- public/js/testing/enemyClassTest.js
+++ public/js/testing/enemyClassTest.js
@@ -4,6 +4,7 @@ const enemyContainer = document.querySelector('.enemyContainer');
 // global variables
 let enemyList = [];
 let weaponList = [];
+let freeSlots = [1, 2, 3, 4, 5];
 
 // enemy names - each enemy will spawn with one of these random names - can be replaced with images later
 const enemyNames = ['Bob Bobberson', 'Test Guy', 'Cool Guy', 'Bad Guy', 'L', 'The guy on the street'];
@@ -11,7 +12,7 @@ const enemyNames = ['Bob Bobberson', 'Test Guy', 'Cool Guy', 'Bad Guy', 'L', 'Th
 // randomly create an enemy
 const createEnemy = function(names, weapons) {
     // use a random name from the names array above
-    const eName = enemyNames[Math.floor(Math.random() * enemyNames.length)];
+    const eName = names[Math.floor(Math.random() * enemyNames.length)];
 
     // uses an instance of the id class. enemyId.use(); increments the id, and returns it
     const eId = enemyId.use();
@@ -20,10 +21,14 @@ const createEnemy = function(names, weapons) {
     const eHealth = Math.floor(Math.random() * 50) + 50;
 
     // all enemies use the same weapon for now
-    const eWeapon = weaponList[0];
+    const eWeapon = weapons[0];
+
+    // get random slot
+    const eSlot = freeSlots[Math.floor(Math.random() * freeSlots.length)];
+    freeSlots.splice(freeSlots.indexOf(eSlot), 1);
 
     // put all the properties together
-    const enemy = new Enemy(eName, eId, eHealth, eWeapon);
+    const enemy = new Enemy(eName, eId, eHealth, eWeapon, eSlot);
 
     return enemy;
 }
@@ -64,7 +69,8 @@ class Player {
 
         if (this.health < 1) {
             console.log(`The player ${this.name} died.`);
-            return true;
+            alert('you died, press ok to refresh!');
+            window.location.reload();
         } else {
             console.log(`The player ${this.name} has ${this.health} health`);
             return false;
@@ -77,16 +83,20 @@ class Player {
         enemyList.forEach(enemy => {
             if (enemy.id === targetId) {
                 // if target found, calculate incoming damage. if calculating returns true, that means the enemy died.
-                if (enemy.calcDmg(weapon.damage) === true) {
+                if (enemy.calcDmg(weapon.damage)) {
                     // loop through enemies until the affected enemy is found
                     for (let i = 0; i < enemyList.length; i++) {
-                        // delete enemy from array & html
+                        // enemy found
                         if (enemyList[i].id === enemy.id) {
-                            enemyList.splice(i, 1);
+                            // delete enemy from document
+                            const delEnemySlot = document.querySelector(`div.slot${enemy.slot}`);
+                            delEnemySlot.innerHTML = '';
+        
+                            // free the slot used by enemy
+                            freeSlots.push(enemyList[i].slot);
 
-                            // delete an enemy
-                            const delEnemy = enemyContainer.querySelector(`#id${enemy.id}`);
-                            delEnemy.outerHTML = null;
+                            // delete enemy from array
+                            enemyList.splice(i, 1);
                         }
                     }
                 };
@@ -97,11 +107,12 @@ class Player {
 
 // enemy class
 class Enemy {
-    constructor(name, id, health, weapon) {
+    constructor(name, id, health, weapon, slot) {
         this.name = name;
         this.id = id;
         this.health = health;
         this.weapon = weapon;
+        this.slot = slot; // used to store which slot the enemy is located in
     }
 
     // handle incoming damage (returns true/false if the enemy dies)
@@ -109,10 +120,10 @@ class Enemy {
         this.health -= dmg;
 
         if (this.health < 1) {
-            console.log(`The enemy ${this.name} died!`);
-            return true;
+            // enemy died
+            return this.slot;
         } else {
-            console.log(`The enemy ${this.name} has ${this.health} hp`);
+            // enemy survived
             return false;
         }
     }
@@ -127,10 +138,10 @@ class Enemy {
 const enemyId = new Id(-1);
 
 // create weapon instances
-weaponList.push(new Weapon('G-18', 50));
+weaponList.push(new Weapon('G-18', 75));
 
 // create player instance
-const player = new Player('Player', 100, weaponList[0]);
+const player = new Player('Player', 1000, weaponList[0]);
 
 // log enemies & player
 console.log(player);
@@ -138,15 +149,35 @@ console.log(weaponList);
 
 // spawn enemies
 function spawnEnemy() {
-    enemyList.push(createEnemy(enemyNames, weaponList));
+    if (freeSlots.length) {
+        enemyList.push(createEnemy(enemyNames, weaponList));
 
-    const htmlTemp = `
-        <button id="id${enemyId.id}" class="enemy" onclick="player.use(player.weapon, ${enemyId.id})">${enemyList[enemyList.length - 1].name}</button>
-    `;
+        const currEnemy = enemyList[enemyList.length - 1];
 
-    enemyContainer.innerHTML += htmlTemp;
+        const htmlTemp = `
+            <button id="id${enemyId.id}" class="enemy" onclick="player.use(player.weapon, ${enemyId.id})">${currEnemy.name}</button>
+        `;
 
-    return enemyList;
+        // put enemy into correct slot
+        const enemySlot = document.querySelector(`div.slot${currEnemy.slot}`);
+
+        enemySlot.innerHTML = htmlTemp;
+
+        setInterval(() => {
+            let enemyAlive;
+            enemyList.forEach(listedEnemy => {
+                if (currEnemy.id === listedEnemy.id) {
+                    enemyAlive = true;
+                }
+            });
+
+            if (enemyAlive) {
+                currEnemy.use(currEnemy.weapon);
+            }
+        }, 1500);
+
+        return enemyList;
+    }
 }
 
-setInterval(spawnEnemy, 750);
+setInterval(spawnEnemy, 1000);
