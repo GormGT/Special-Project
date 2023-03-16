48d0bb67b1b0ad3234dc0c24faf46ef4d5fc9674
Aksel <aksel4455@gmail.com>
2023-03-16 08:24:00 +0100

    small fixes to login and signup pages

--- controllers/controller.js
+++ controllers/controller.js
@@ -31,16 +31,20 @@ module.exports.home_get = (req, res) => {
 }
 
 module.exports.login_get = (req, res) => {
-    res.render("login");
+    res.render("login", { title: "Log In", bg: "main"});
 }
 
+// l o g i n   p o s t   r e q u e s t
 module.exports.login_post = async (req, res) => {// login
     const { username, email, password } = req.body;
     
     try {
+        // makes user model
         const user = await User.login(username, email, password);
+        // makes jwt token
         const token = createToken(user._id);
-        res.cookie("jwt", token, { httpOnly : true, maxAge : maxAge * 1000 })
+        // saves token to cookies
+        res.cookie("jwt", token, { httpOnly : true, maxAge : maxAge * 1000 });
         res.status(200).json({ user : user._id });
     } catch (err) {
         res.status(400).json({ errors });
@@ -49,15 +53,19 @@ module.exports.login_post = async (req, res) => {// login
 }
 
 module.exports.signup_get = (req, res) => {
-    res.render("signup");
+    res.render("signup", { title: "Sign Up", bg: "main"});
 }
 
+// signup post request
 module.exports.signup_post = async (req, res) => {// signup
     const { username, email, password } = req.body;
 
     try {
+        // creates user from schema
         const user = await User.create({ username, email, password });
+        // creates logged in json web token
         const token = createToken(user._id);
+        // saves token to browser
         res.cookie("jwt", token, { httpOnly : true, maxAge : maxAge * 1000 })
         res.status(201).json({ user : user._id });
     } catch (err) {
@@ -69,12 +77,12 @@ module.exports.signup_post = async (req, res) => {// signup
 
 // temp
 module.exports.audiotest_get = (req, res) => {
-    res.render("audiotest");
+    res.render("audiotest", { title: "Audio Test", bg: "main"});
 }
 
-module.exports.enemyClassTest_get = (req, res) => res.render("enemyClassTest");
+module.exports.enemyClassTest_get = (req, res) => res.render("enemyClassTest", { title: "Enemy Class Test", bg: "main"});
 
 // 404
 module.exports.error404 = (req, res) => {
-    res.render("404");
+    res.render("404", { title: "404 page not found", bg: "main"});
 }
